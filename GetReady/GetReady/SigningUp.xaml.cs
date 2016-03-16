816bb0999731677df6c1c3c6eb1b6273b5b16e06
Alexandra Stankevich <abstankevich@edu.hse.ru>
2016-03-16 18:05:41 +0400

    DB4

--- GetReady/GetReady/SigningUp.xaml.cs
+++ GetReady/GetReady/SigningUp.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Data;
+//using System.Data;
 namespace GetReady
 {
     /// <summary>
@@ -53,46 +53,28 @@ namespace GetReady
 
         //private void LetsStart_Click(object sender, RoutedEventArgs e)
         //{
-        //    UsersEntities db = new UsersEntities();
+            //    UsersEntities db = new UsersEntities();
 
-        //    db.UserTable.Add(new UserTable
-        //    {
-        //        username = Username.Text,
-        //        hash = GetHash(Password.Text)
-        //    });
-        //    db.SaveChanges();
-        //    StartPage StartPage = new StartPage();
-        //    StartPage.Show();
-        //}
-        //public string GetHash(string password)
-        //{ 
-        //    System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
-        //    string hash = String.Empty; byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
-        //    foreach (byte theByte in crypto)
-        //    { hash += theByte.ToString("x2"); }
-        //    return hash; }
+            //    db.UserTable.Add(new UserTable
+            //    {
+            //        username = Username.Text,
+            //        hash = GetHash(Password.Text)
+            //    });
+            //    db.SaveChanges();
+            //    StartPage StartPage = new StartPage();
+            //    StartPage.Show();
+            //}
+            //public string GetHash(string password)
+            //{ 
+            //    System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
+            //    string hash = String.Empty; byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
+            //    foreach (byte theByte in crypto)
+            //    { hash += theByte.ToString("x2"); }
+            //    return hash; }
 
-        //private void GotFocusUsername(object sender, RoutedEventArgs e)
-        //{
-        //    UsersEntities db = new UsersEntities();
-            
-        //    db.UserTable.Add(new UserTable
-        //    {
-        //        username = Username.Text,
-        //        hash = GetHash(Password.Text)
-        //    });
-        //    db.SaveChanges();
-        //    StartPage StartPage = new StartPage();
-        //    StartPage.Show();
-        //}
-        //public string GetHash(string password)
-        //{ 
-        //    System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
-        //    string hash = String.Empty; byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
-        //    foreach (byte theByte in crypto)
-        //    { hash += theByte.ToString("x2"); }
-        //    return hash; }
-    }
-    
+            StartPage Startpage = new StartPage();
+            Startpage.Show();
+        }
+
     }
 
