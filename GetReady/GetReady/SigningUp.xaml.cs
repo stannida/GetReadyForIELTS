a592c1bf60d9f1bd9d7aa6fe528a6611a913fe77
Volgina Sofiya <svvolgina@edu.hse.ru>
2016-03-16 16:47:06 +0300

    Change

--- GetReady/GetReady/SigningUp.xaml.cs
+++ GetReady/GetReady/SigningUp.xaml.cs
@@ -24,27 +24,34 @@ namespace GetReady
             InitializeComponent();
         }
 
-        private void LetsStart_Click(object sender, RoutedEventArgs e)
+        //private void LetsStart_Click(object sender, RoutedEventArgs e)
+        //{
+        //    UsersEntities db = new UsersEntities();
+
+        //    db.UserTable.Add(new UserTable
+        //    {
+        //        username = Username.Text,
+        //        hash = GetHash(Password.Text)
+        //    });
+        //    db.SaveChanges();
+        //    StartPage StartPage = new StartPage();
+        //    StartPage.Show();
+        //}
+        //public string GetHash(string password)
+        //{ 
+        //    System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
+        //    string hash = String.Empty; byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
+        //    foreach (byte theByte in crypto)
+        //    { hash += theByte.ToString("x2"); }
+        //    return hash; }
+
+        private void GotFocusUsername(object sender, RoutedEventArgs e)
         {
-            UsersEntities db = new UsersEntities();
-            
-            db.UserTable.Add(new UserTable
-            {
-                username = Username.Text,
-                hash = GetHash(Password.Text)
-            });
-            db.SaveChanges();
-            StartPage StartPage = new StartPage();
-            StartPage.Show();
+            if (Username.Text == "")
+                Username.Text = "";
+
         }
-        public string GetHash(string password)
-        { 
-            System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
-            string hash = String.Empty; byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
-            foreach (byte theByte in crypto)
-            { hash += theByte.ToString("x2"); }
-            return hash; }
     }
-    
-    }
+}
+
 
